83a6af7138a912b37fb0956f56a577d042c4f064
genellepalana <genellepalana@gmail.com>
2024-01-27 23:18:49 -0800

    added sass to project
    -re-installed sass into react project
    - created sass folder with partial files
    - connected sass to index.js
    - NOTE: include in ur terminal: npm install

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
+import './sass/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
