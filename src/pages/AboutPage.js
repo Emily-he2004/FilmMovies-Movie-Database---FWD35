8615156e806b4a50cc06f0f9fa19e146942a2789
genellepalana <genellepalana@gmail.com>
2024-01-27 19:43:27 -0800

    added additional partial files & content to about page
    -added in partial files: home, favorites, singles, and about.
    
    - added text content to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,8 @@ function AboutPage() {
   return (
     <div>
       <h2>This is the About Page.</h2>
+      <p>Here at FMovies, we strive to become the largest movie database so that you won't ever need to search elsewhere.</p>
+      <p>This website uses TMDB API but is not endorsed nor certified by TMDB</p>
     </div>
   );
 }
