8672886eba7b512ca66c9af5df6a7ef8fdebfa25
David <149295375+D-Zhan@users.noreply.github.com>
2024-01-28 20:43:01 -0800

    Header Navigation Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
+import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import movieMiniLogo from "../media/movie_mini_logo.svg";
+import movieFullLogo from "../media/movie_full_logo.svg";
+import { useState } from "react";
 
 function Header() {
+
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div>
+    <header id="navigation-wrapper" className={`${showNav ? "show" : ""}`}>
+      <div className="header-container">
+        <Link className="movie-logo" to="/">
+          <img src={movieMiniLogo} alt="Movie Logo" />
+        </Link>
+        <button onClick={() => { setShowNav(!showNav) }} className="hamburger">☰</button>
+      </div>
       <NavBar />
-      <SearchBar
-        // searchMovie={searchMovie}
-        // setSearchMovie={setSearchMovie}
-        // fetchMovieData={fetchMovieData}
-      />
-    </div>
+      {/* <SearchBar
+      // searchMovie={searchMovie}
+      // setSearchMovie={setSearchMovie}
+      // fetchMovieData={fetchMovieData}
+      /> */}
+
+      {/* Commented out SearchBar for the time being */}
+    </header>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,36 +2,42 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>
-      <div>
-        <Link to="/">
-          <img
-            className="full-logo"
-            src="./media/movie_full_logo.svg"
-            // movie app LOGOS - both full and mini versions go HERE.
-            // Links to "HomePage" <Route />
-            // Need to only show ONE of 2 logos depending on screen size.
-          />
-          <img className="mini-logo" src="./media/movie_mini_logo.svg" />
-        </Link>
-      </div>
-      {/* Does the button need it's own <div> or
-                Can it go inside menu list's <div>? */}
-      <div>
-        <button>
-          <img button image goes here />
-        </button>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/favourites">Favourites</Link>
-          </li>
-        </ul>
-      </div>
-      {/* Search bar must go inside <Header /> */}
-    </div>
+    // <div>
+    //   <div>
+    //     <Link to="/">
+    //       <img
+    //         className="full-logo"
+    //         src="./media/movie_full_logo.svg"
+    //         // movie app LOGOS - both full and mini versions go HERE.
+    //         // Links to "HomePage" <Route />
+    //         // Need to only show ONE of 2 logos depending on screen size.
+    //       />
+    //       <img className="mini-logo" src="./media/movie_mini_logo.svg" />
+    //     </Link>
+    //   </div>
+    //   {/* Does the button need it's own <div> or
+    //             Can it go inside menu list's <div>? */}
+    //   <div>
+    //     <button>
+    //       <img button image goes here />
+    //     </button>
+    //     <ul>
+    //       <li>
+    //         <Link to="/about">About</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/favourites">Favourites</Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   {/* Search bar must go inside <Header /> */}
+    // </div>
+    <nav className="header-nav">
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/favourites">Favourites</Link></li>
+      </ul>
+    </nav>
   );
 }
 
