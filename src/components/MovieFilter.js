459e8bfa78723d1bd8c74e65122fda26891e07f2
David <149295375+D-Zhan@users.noreply.github.com>
2024-01-29 20:06:32 -0800

    Category Filter Styling Updated

--- src/components/MovieFilter.js
+++ src/components/MovieFilter.js
@@ -4,17 +4,17 @@ function MovieFilter({ filter, setFilter }) {
   return (
     <div className="movieCategories">
       <ul>
-        <li>
+        <li className="filter-popular">
           <button className="filterBtn" onClick={() => setFilter("popular")}>Popular</button>
         </li>
-        <li>
+        <li className="filter-top-rated">
           <button className="filterBtn" onClick={() => setFilter("top_rated")}>Top-Rated</button>
         </li>
-        <li>
+        <li className="filter-now-playing">
           <button className="filterBtn" onClick={() => setFilter("now_playing")}>Now Playing</button>
         </li>
-        <li>
-          <button className="filterBtn" onClick={() => setFilter("upcoming")}>Upcoming</button>
+        <li className="filter-upcoming">
+          <button className="filterBtn" onClick={() => setFilter("upcoming")}>UpComing</button>
         </li>
       </ul>
     </div>
